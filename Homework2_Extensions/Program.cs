7d6d06e7604364b76356acedaf3075b60d70ead7
Muhammet Mustafa ÖZTÜRK <77894227+mustafaozturk34@users.noreply.github.com>
2021-12-02 18:05:09 +0300

    first commit

---
+++ Homework2_Extensions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework2_Extensions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
